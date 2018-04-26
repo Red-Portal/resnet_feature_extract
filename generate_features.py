d9b4e840fd84b4b29b57be30778426ec4015f019
Red-Portal <msca8h@naver.com>
2018-04-26 14:27:46 +0900

    gogo

--- generate_features.py
+++ generate_features.py
@@ -58,7 +58,7 @@ def main():
         num_parts           = kv.num_workers,
         part_index          = kv.rank)
 
-    out_layer = sym.get_internals().list_outputs()["flatten0_output"]
+    out_layer = sym.get_internals().list_outputs()[-6]
     model = mx.mod.Module(
         out_layer,
         context             = ctx,
