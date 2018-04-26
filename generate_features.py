4acfd4dc985da557adbb8ac31460d2aea07024e3
Red-Portal <msca8h@naver.com>
2018-04-26 14:52:54 +0900

    bind shove

--- generate_features.py
+++ generate_features.py
@@ -21,7 +21,7 @@ def evaluate(ctx, sym, data):
         data = batch.data[0]
         label = batch.label[0].asnumpy()
 
-        executor = out_layer.bind(ctx=ctx, args={"data": data})
+        executor = sym.bind(ctx=ctx, args={"data": data})
         executor.forward()
         out = executor.outputs.asnumpy()
         fmaps.append(out)
