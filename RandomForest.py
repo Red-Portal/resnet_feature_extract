49987c8e6d384c09f7ab097339574edc0332afdc
Red-Portal <msca8h@naver.com>
2018-04-27 17:53:07 +0900

    added stuff

--- RandomForest.py
+++ RandomForest.py
@@ -12,13 +12,10 @@ def main():
     val_x = np.load("val_fmap.npy")
     val_y = np.load("val_labels.npy")
 
-    train_mat = xgb.DMatrix(train_x, label=train_y)
-    val_mat = xgb.DMatrix(val_x, label=val_y)
-
     forest = RandomForestClassifier(
-        n_estimators=100,
+        n_estimators=args.trees,
         criterion="entropy",
-        max_depth=10,
+        max_depth=args.max_depth,
         oob_score=False,
         n_jobs=6,
         verbose=3)
@@ -32,10 +29,7 @@ def main():
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="command line parameters")
-    parser.add_argument('--max-depth', type=int, default=2)
-    parser.add_argument('--eta', type=float, default=0.2)
-    parser.add_argument('--rounds', type=int, default=500)
-    parser.add_argument('--lambda_reg', type=float, default=3.0)
-    parser.add_argument('--subsample', type=float, default=0.1)
+    parser.add_argument('--max-depth', type=int, default=10)
+    parser.add_argument('--trees', type=float, default=100)
     args = parser.parse_args()
     main()
